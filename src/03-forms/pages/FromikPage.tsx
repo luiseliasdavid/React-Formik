72ef5ce46f2e209080545d0c9d5899e46d5a68f6
luiseliasdavid <luis.elias.david@gmail.com>
2023-04-07 21:58:05 -0300

    yup

--- src/03-forms/pages/FromikPage.tsx
+++ src/03-forms/pages/FromikPage.tsx
@@ -1,17 +1,48 @@
 import { useFormik } from "formik";
+import { FormikErrors } from "formik/dist/types";
 
 import "../styles/styles.css";
 
+interface FormValues {
+    firstName: string;
+    lastName: string;
+    emailAddress: string;
+}
+
 export const FromikBasicPage = () => {
-  const {handleChange, values, handleSubmit} = useFormik({
+    
+    const validate = ({firstName,lastName,emailAddress}:FormValues) => {
+      const errors: FormikErrors<FormValues>= {}
+
+      if (!firstName) {
+        errors.firstName= 'required';
+      } else if (firstName.length > 15){
+        errors.firstName = 'must be 15 characters or less'   
+      }
+      if (!lastName) {
+        errors.lastName= 'required';
+      } else if (lastName.length > 10){
+        errors.lastName = 'must be 10 characters or less'   
+      }
+      if (!emailAddress) {
+        errors.emailAddress = 'Required';
+      } else if (!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(emailAddress)) {
+        errors.emailAddress = 'Invalid email address';
+      }
+
+      return errors
+  }
+
+  const { handleChange, values, handleSubmit, errors, touched, handleBlur } = useFormik({
     initialValues: {
       firstName: "",
       lastName: "",
       emailAddress: "",
-      },
-    onSubmit : (values) => {
+    },
+    onSubmit: (values) => {
       console.log(values);
     },
+    validate
   });
 
   return (
@@ -20,32 +51,35 @@ export const FromikBasicPage = () => {
 
       <form noValidate onSubmit={handleSubmit}>
         <label htmlFor="firstName">First Name</label>
-        <input 
-        type="text" 
-        name="firstName" 
-        onChange={handleChange} 
-        value={values.firstName}
+        <input
+          type="text"
+          name="firstName"
+          onBlur={handleBlur}
+          onChange={handleChange}
+          value={values.firstName}
         />
-        <span>First Name is required</span>
+        { touched.firstName && errors.firstName && <span>{errors.firstName }</span>}
 
         <label htmlFor="lastName">Last Name</label>
-        <input 
-        type="text" 
-        name="lastName" 
-        onChange={handleChange}
-        value={values.lastName}
+        <input
+          type="text"
+          name="lastName"
+          onBlur={handleBlur}
+          onChange={handleChange}
+          value={values.lastName}
         />
-        <span>Last Name is required</span>
+        {touched.lastName && errors.lastName && <span> {errors.lastName} </span>}
 
         <label htmlFor="emailAddress">Email Address</label>
-        <input 
-        type="emailAddress" 
-        name="emailAddress" 
-        onChange={handleChange}
-        value={values.emailAddress}
+        <input
+          type="emailAddress"
+          name="emailAddress"
+          onBlur={handleBlur}
+          onChange={handleChange}
+          value={values.emailAddress}
         />
-        <span>Email Address is required</span>
-        <span>Check for a valid email format</span>
+        { touched.emailAddress && errors.emailAddress && <span> {errors.emailAddress} </span>}
+       
 
         <button type="submit">Submit</button>
       </form>
--- src/routes/Navigation.tsx
+++ src/routes/Navigation.tsx
@@ -1,6 +1,8 @@
 import { BrowserRouter } from 'react-router-dom';
 import { Routes, Route, NavLink, Navigate } from 'react-router-dom';
+import { FromikComponents } from '../03-forms/pages/FromikComponents';
 import { FromikBasicPage } from '../03-forms/pages/FromikPage';
+import { FromikYupPage } from '../03-forms/pages/FromikYupPage';
 import { RegisterPage } from '../03-forms/pages/RegisterPage';
 
 import logo from '../logo.svg'
@@ -19,6 +21,12 @@ export const Navigation = () => {
                             <NavLink to="/formik-basic" className={ ({ isActive }) => isActive ? 'nav-active' : '' }>Formik Basic</NavLink>
                         </li>
                         <li>
+                            <NavLink to="/formik-yup" className={ ({ isActive }) => isActive ? 'nav-active' : '' }>Formik yup</NavLink>
+                        </li>
+                        <li>
+                            <NavLink to="/formik-components" className={ ({ isActive }) => isActive ? 'nav-active' : '' }>Formik Components</NavLink>
+                        </li>
+                        <li>
                             <NavLink to="/users" className={ ({ isActive }) => isActive ? 'nav-active' : '' }>Users</NavLink>
                         </li>
                     </ul>
@@ -28,6 +36,8 @@ export const Navigation = () => {
                 <Routes>
                     <Route path="/register" element={ <RegisterPage/> } />
                     <Route path="/formik-basic" element={ <FromikBasicPage/> } />
+                    <Route path="/formik-yup" element={ <FromikYupPage/> } />
+                    <Route path="/formik-components" element={ <FromikComponents/> } />
                     <Route path="home" element={ <h1>Home Page</h1> } />
                     
                     <Route path="/*" element={ <Navigate to="/home" replace /> } />
